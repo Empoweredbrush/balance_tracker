ba230a395ed3bd0ffc39c5855e4e8a0792148386
Empoweredbrush <Empoweredbrushfour@gmail.com>
2025-02-19 18:35:04 -0700

    Table created and program getting started

---
+++ balance_tracker.py
@@ -0,0 +1,63 @@
+import sqlite3
+
+conn = sqlite3.connect("balance_tracker.db")
+
+print("Connected successfully!")
+
+"""
+conn.execute('''CREATE TABLE EXPENSES
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AMOUNT          DECIMAL(65, 2)    NOT NULL,
+         DESCRIPTION     CHAR(50));''')
+
+print("Created Table Successfully!")
+"""
+
+
+def Start():
+    print("Welcome to your balance tracker! This program was created to help you in tracking your balance and expenses.")
+    print("To start will you be adding a new expense, updating an expense, deleting an expense, or looking for an expense? (use A for new, B for update, C for delete, and D for viewing one expense):")
+
+    answer = input()
+
+    #if (answer.lower() == "a"):
+       # Add()
+
+def Add():
+    print("Welcome to the add part of the program! We will be adding a new expense to your tracker but need some info.")
+    idList = []
+
+    cur = conn.cursor()
+
+    cur.execute('''SELECT ID FROM EXPENSES''')
+    rows = cur.fetchall()
+
+    for row in rows:
+        idList.append(row)
+
+    print("To start, please give the name of the expense:")
+
+    name = input()
+
+    print("Now, please put down the amount (Note, after the decimal max of two numbers, Example: 2.50):")
+
+    amount = input()
+
+    print("Next, add a very very short description, 50 character limit, or type no to leave blank:")
+
+    answer = input()
+
+    description = None
+
+    if (answer.lower() != "no"):
+        description = answer
+    
+    conn.execute('''INSERT INTO EXPENSES (ID, NAME, AMOUNT, DESCRIPTION)
+                 VALUES ()''')
+
+#def Update():
+
+#def Delete():
+
+#def View():
